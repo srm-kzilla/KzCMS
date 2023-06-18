da08d85f8fedef9e92b04fc62f8c702cd5e7d6f3
Ash <01tripster@gmail.com>
2023-06-19 04:00:16 +0530

    chore: delete duplicate files (#26)

--- src/api/projects/projects.controller.ts
+++ src/api/projects/projects.controller.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from 'express';
-import { handleCreateProject, handleCreateProjects, handleGetAllProjects, handleGetProject } from './projects.service';
+import {
+  handleCreateProject,
+  handleCreateProjects,
+  handleGetAllProjects,
+  handleGetProject,
+  handleDeleteImage,
+  handleDeleteProject,
+  handlePostImage,
+  handleUpdateProject,
+} from './projects.service';
 
 export const getProjects = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -78,6 +87,93 @@ export const createProjects = async (req: Request, res: Response): Promise<void>
     res.status(500).json({
       success: true,
       message: 'Unable to get projects',
+    });
+  }
+};
+
+export const updateProject = async (
+  req: Request & {
+    body: {
+      title: string;
+      description: string;
+      link: string;
+      author: string;
+    };
+  },
+  res: Response,
+): Promise<void> => {
+  try {
+    const data = await handleUpdateProject(req.params.slug, req.body);
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(`Error while updating Project: ${error}`);
+    res.status(500).json({
+      success: false,
+      message: `Error while updating Project: ${error}`,
+    });
+  }
+};
+
+export const deleteProject = async (
+  req: Request & {
+    body: {
+      slug: string;
+    };
+  },
+  res: Response,
+): Promise<void> => {
+  try {
+    const data = await handleDeleteProject(req.params.slug);
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(`Error while updating Project: ${error}`);
+    res.status(500).json({
+      success: false,
+      message: `Error while updating Project: ${error}`,
+    });
+  }
+};
+
+export const postImage = async (
+  req: Request & {
+    body: {
+      slug: string;
+      title: string;
+      image: File;
+    };
+  },
+  res: Response,
+): Promise<void> => {
+  try {
+    const data = await handlePostImage(req.body);
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(`Error while updating Project: ${error}`);
+    res.status(500).json({
+      success: false,
+      message: `Error while updating Project: ${error}`,
+    });
+  }
+};
+
+export const deleteImage = async (
+  req: Request & {
+    body: {
+      slug: string;
+      title: string;
+      imageUrl: string;
+    };
+  },
+  res: Response,
+): Promise<void> => {
+  try {
+    const data = await handleDeleteImage(req.body.slug, req.body.title, req.body.imageUrl);
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(`Error while updating Project: ${error}`);
+    res.status(500).json({
+      success: false,
+      message: `Error while updating Project: ${error}`,
     });
   }
 };
--- src/api/projects/projects.service.ts
+++ src/api/projects/projects.service.ts
@@ -3,6 +3,23 @@ interface Project {
   name: string;
 }
 
+interface ImageData {
+  slug: string;
+  title: string;
+  image: File;
+}
+
+interface UpdatedProjectData {
+  slug: string;
+  data: {
+    title: string;
+    description: string;
+    imageURL: string;
+    link: string;
+    author: string;
+  };
+}
+
 interface ProjectData {
   slug: string;
   data: {
@@ -28,4 +45,36 @@ export const handleCreateProject = async (project: Project): Promise<Project> =>
 
 export const handleCreateProjects = async (): Promise<unknown[]> => {
   return [];
+};
+
+export const handleUpdateProject = async (
+  slug: string,
+  data: {
+    title: string;
+    description: string;
+    imageURL: string;
+    link: string;
+    author: string;
+  },
+): Promise<UpdatedProjectData> => {
+  //update the project here
+  return { slug, data };
+};
+
+export const handleDeleteProject = async (slug: string): Promise<unknown> => {
+  //delete a specific project
+  return { success: true, message: `${slug} deleted` };
+};
+
+export const handlePostImage = async (data: {
+  slug: string;
+  title: string;
+  image: File;
+}): Promise<{ data: ImageData }> => {
+  // post an image
+  return { data };
+};
+
+export const handleDeleteImage = async (slug: string, title: string, imageUrl: string): Promise<unknown> => {
+  return { success: true, message: `${imageUrl} deleted from ${title} of project ${slug}` };
 };
