de17fd119abd4292790d63df2d5207735395ab2d
Ash <01tripster@gmail.com>
2023-06-19 03:25:01 +0530

    chore: rename projects files

--- src/api/projects/projects.service.ts
+++ src/api/projects/projects.service.ts
@@ -1,9 +1,4 @@
-interface Project {
-  slug: string;
-  name: string;
-}
-
-interface ProjectData {
+interface UpdatedProjectData {
   slug: string;
   data: {
     title: string;
@@ -11,21 +6,43 @@ interface ProjectData {
     imageURL: string;
     link: string;
     author: string;
-  }[];
+  };
 }
 
-export const handleGetAllProjects = async (): Promise<Project[]> => {
-  return [];
+interface ImageData {
+  slug: string;
+  title: string;
+  image: File;
+}
+
+export const handleUpdateProject = async (
+  slug: string,
+  data: {
+    title: string;
+    description: string;
+    imageURL: string;
+    link: string;
+    author: string;
+  },
+): Promise<UpdatedProjectData> => {
+  //update the project here
+  return { slug, data };
 };
 
-export const handleGetProject = async (slug: string): Promise<ProjectData> => {
-  return { slug, data: [] };
+export const handleDeleteProject = async (slug: string): Promise<unknown> => {
+  //delete a specific project
+  return { success: true, message: `${slug} deleted` };
 };
 
-export const handleCreateProject = async (project: Project): Promise<Project> => {
-  return project;
+export const handlePostImage = async (data: {
+  slug: string;
+  title: string;
+  image: File;
+}): Promise<{ data: ImageData }> => {
+  // post an image
+  return { data };
 };
 
-export const handleCreateProjects = async (): Promise<unknown[]> => {
-  return [];
+export const handleDeleteImage = async (slug: string, title: string, imageUrl: string): Promise<unknown> => {
+  return { success: true, message: `${imageUrl} deleted from ${title} of project ${slug}` };
 };
