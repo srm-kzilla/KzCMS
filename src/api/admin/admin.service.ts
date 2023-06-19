b1a1cfd38817266f333232ee46b05861daf88017
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-06-19 08:45:31 +0530

    feat: verify endpoint

--- src/api/admin/admin.service.ts
+++ src/api/admin/admin.service.ts
@@ -27,6 +27,4 @@ export const handleVerifyUser = (): UserWithVerification => {
   };
 };
 
-export const handleDeleteUser = (): void => {
-  console.log('User Deleted');
-};
+export const handleDeleteUser = (): void => {};
