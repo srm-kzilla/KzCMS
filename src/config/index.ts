83d7b3f79350e7f6c4e88e2baae30f789135ce6b
Shashank Kumar <shashankkumarthakur@gmail.com>
2023-06-19 17:56:08 +0530

    fix: zod schema fixed (#40)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,13 @@
-require('dotenv').config();
+import { config } from 'dotenv';
 import { z } from 'zod';
+
+config();
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']).default('development'),
-  port: z.number().int().positive().optional().default(5050),
-  databaseURL: z.string().startsWith('mongodb+srv://').includes('mongodb.net'),
-  JWT_SECRET: z.string().regex(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/),
+  PORT: z.string().regex(/^\d+$/).optional().default("5050"),
+  MONGODB_URI: z.string().startsWith('mongodb+srv://').includes('mongodb.net'),
+  JWT_SECRET: z.string(),
   level: z.enum(['error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly']).optional().default('silly'),
   prefix: z.string().optional().default('/api'),
 });
@@ -15,9 +17,9 @@ const parsedSchema = envSchema.parse(process.env);
 export default {
   NODE_ENV: parsedSchema.NODE_ENV,
 
-  port: parsedSchema.port,
+  PORT: parsedSchema.PORT,
 
-  databaseURL: parsedSchema.databaseURL,
+  MONGODB_URI: parsedSchema.MONGODB_URI,
 
   JWT_SECRET: parsedSchema.JWT_SECRET,
 
