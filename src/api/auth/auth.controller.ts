21f3645b7f74c9450573bdeb989fe9492d90fd83
shawshankkumar <shashankkumarthakur@gmail.com>
2023-06-21 18:57:53 +0530

    chore: update flow of code for login

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { handleAddNewUser, handleExistingUser } from './auth.service';
 import { authParamType } from './auth.schema';
 import Logger from '../../loaders/logger';
+import LoggerInstance from '../../loaders/logger';
 
 export const addNewUser = async (req: Request, res: Response) => {
   const email = req.body.email;
@@ -15,11 +16,17 @@ export const addNewUser = async (req: Request, res: Response) => {
 export const loginExistingUser = async (req: Request, res: Response) => {
   try {
     const userObj: authParamType = { email: req.body.email, password: req.body.password };
-
-    const data = await handleExistingUser(userObj);
-    res.status(data.status).json(data);
+    const token = await handleExistingUser(userObj);
+    res.status(200).json({
+      success: true,
+      message: 'Login Successful',
+      token,
+    });
   } catch (err) {
-    Logger.error(err);
-    return { status: 500, message: 'Internal Server Error' };
+    LoggerInstance.error(err);
+    res.status(err.statusCode ?? 500).json({
+      success: false,
+      message: err.message ?? 'Internal Server Error'
+    })
   }
 };
