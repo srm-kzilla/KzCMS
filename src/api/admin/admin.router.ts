8d612839c0c4a27cd9f37dc7118e36fa6b827d3a
Jatin Kumar <main.jatink@gmail.com>
2023-06-21 05:42:48 +0530

    fix: change request method to patch

--- src/api/admin/admin.router.ts
+++ src/api/admin/admin.router.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { deleteUser, getUsers, updateUser, updateUserProjects, verifyUser } from './admin.controller';
 import { validateRequest } from '../../shared/middlewares/validator';
-import { verifyUserSchema } from '../../types/admin/admin-schema';
+import { verifyUserSchema } from '../../types/admin/admin.schema';
 
 export default (): Router => {
   const app = Router();
 
+  app.patch('/verify', validateRequest('body', verifyUserSchema), verifyUser);
   app.patch('/:user', updateUserProjects);
-  app.put('/verify', validateRequest('body', verifyUserSchema), verifyUser);
   app.get('/', getUsers);
   app.patch('/:user', updateUser);
   app.delete('/:user', deleteUser);
