6fd4dba58e03d018495d9750b82e1a138de2611b
Aditya Gupta <adityag860libra@gmail.com>
2023-06-21 17:02:02 +0530

    chore: updated auth controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,11 +1,17 @@
 import { Router, Request, Response } from 'express';
 import { handleAddNewUser, handleLoginUser } from './auth.service';
+import LoggerInstance from '../../loaders/logger';
+
 
 export const addNewUser = async (req: Request, res: Response) => {
   const user = req.body;
+  try{
   const newUser = await handleAddNewUser(user);
-
-  return res.status(newUser.status).send(newUser);
+  res.status(newUser.status).send(newUser);
+  }catch(error){
+    LoggerInstance.error(error);
+    res.status(error.statusCode ?? 500).json({ success: false, message: error.message ?? 'internal server error' });
+  }
 };
 
 export const loginExistingUser = async (req: Request, res: Response) => {
