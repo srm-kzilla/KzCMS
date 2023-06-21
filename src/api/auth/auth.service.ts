c4259e8b4a234e430dfa203188acf015d1da5ba5
Shashank Kumar <shashankkumarthakur@gmail.com>
2023-06-21 19:11:32 +0530

    fix logic in signup (#55)
    * chore: delete yarn-error.log
    
    * chore: delete jwt.js
    
    * chore: Fix signup code

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,34 +1,21 @@
 import db from '../../loaders/database';
 import bcrypt from 'bcrypt';
 import { userScemaType } from '../types/auth.schema';
-import Logger from '../../loaders/logger';
 
 export async function handleAddNewUser(signup: userScemaType) {
-  try {
-    const data = await (await db()).collection('users').findOne({ email: signup.email });
-    if (data !== null) {
-      return { status: 409, error: 'This email already exists', success: false };
-    }
-    const collection = (await db()).collection('users');
-
-    const saltRounds = 10;
-    const hash = await bcrypt.hash(signup.password, saltRounds);
-    const currentDateTime = new Date();
-
-    await collection.insertOne({
-      email: signup.email,
-      password: hash,
-      created_at: currentDateTime,
-      isAdmin: false,
-      isVerified: false,
-      isDeleted: false,
-    });
-
-    return { status: 200, email: signup.email, error: 'New user added successfully', success: true };
-  } catch (error) {
-    Logger.error(error);
-    throw { status: 500, email: signup.email, message: 'User could not be added', success: false };
+  const data = await (await db()).collection('users').findOne({ email: signup.email });
+  if (!data) {
+    throw { statusCode: 409, message: 'This email already exists', success: false };
   }
+  const collection = (await db()).collection('users');
+
+  const saltRounds = 10;
+  const hash = await bcrypt.hash(signup.password, saltRounds);
+
+  await collection.insertOne({
+    ...signup,
+    password: hash,
+  });
 }
 
 export async function handleLoginUser(login: userScemaType) {
