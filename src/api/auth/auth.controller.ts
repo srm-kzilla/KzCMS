30b2aca96ba41e87be27f46aa718df2787b56dac
shawshankkumar <shashankkumarthakur@gmail.com>
2023-06-21 19:07:47 +0530

    chore: Fix signup code

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -2,13 +2,15 @@ import { Router, Request, Response } from 'express';
 import { handleAddNewUser, handleLoginUser } from './auth.service';
 import LoggerInstance from '../../loaders/logger';
 
-
 export const addNewUser = async (req: Request, res: Response) => {
   const user = req.body;
-  try{
-  const newUser = await handleAddNewUser(user);
-  res.status(newUser.status).json(newUser);
-  }catch(error){
+  try {
+    await handleAddNewUser(user);
+    res.status(200).json({
+      success: true,
+      message: 'User created successfully',
+    });
+  } catch (error) {
     LoggerInstance.error(error);
     res.status(error.statusCode ?? 500).json({ success: false, message: error.message ?? 'Internal server error' });
   }
