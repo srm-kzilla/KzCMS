358761565ffe53c7299f3f3cc850c8e5090bbee7
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-06-26 23:57:27 +0530

    chore: add suitable validate middleware schema

--- src/api/admin/admin.router.ts
+++ src/api/admin/admin.router.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 import { deleteUser, getUsers, updateUser, updateUserProjects, verifyUser } from './admin.controller';
 import { validateRequest } from '@/shared/middlewares/validator';
-import { VerifyUserSchema, DeleteUserSchema } from '@/shared/types/admin/admin.schema';
-import { LoginSchema } from '@/shared/types/auth/auth.schema';
+import { VerifyUserSchema, DeleteUserSchema, AuthSchema } from '@/shared/types/admin/admin.schema';
 
 export default (): Router => {
   const app = Router();
@@ -10,7 +9,7 @@ export default (): Router => {
   app.patch('/verify', validateRequest('body', VerifyUserSchema), verifyUser);
   app.patch('/:user', updateUserProjects);
   app.get('/', getUsers);
-  app.patch('/user/update', validateRequest('body', LoginSchema), updateUser);
+  app.patch('/user/update', validateRequest('body', AuthSchema), updateUser);
   app.delete('/user', validateRequest('body', DeleteUserSchema), deleteUser);
 
   return app;
