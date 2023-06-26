23f68700818db0d9c25caba6eea101d8de972c07
Harsh Patel <harshnpatel9@gmail.com>
2023-06-26 12:38:36 +0530

    remove comments

--- src/api/projects/projects.router.ts
+++ src/api/projects/projects.router.ts
@@ -9,10 +9,7 @@ export default (): Router => {
   app.get('/:slug', authenticateToken(), getProject);
 
   // TODO: NEED TO DO THIS IMAGES AFTER THE NEXT MEET
-  // app.post('/image', authenticateToken(), postImage);
-  // app.delete('/image', authenticateToken(), deleteImage); // ig it's update images now
 
-  // We Need to add a middleware to check if the user is admin for below routes
   app.post('/', authenticateToken({ verifyAdmin: true }), createProject);
   app.patch('/:slug', authenticateToken({ verifyAdmin: true }), updateProject);
   app.delete('/:slug', authenticateToken({ verifyAdmin: true }), deleteProject);
