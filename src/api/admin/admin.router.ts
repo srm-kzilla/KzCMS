8fadef25509d2b00f89b52bc58944e2c9dfd6a53
Ash <01tripster@gmail.com>
2023-06-26 22:09:35 +0530

    fix: add parameter validation

--- src/api/admin/admin.router.ts
+++ src/api/admin/admin.router.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { deleteUser, getUserDetails, getUsers, updateUser, updateUserProjects, verifyUser } from './admin.controller';
 import { validateRequest } from '@/shared/middlewares/validator';
-import { verifyUserSchema, deleteUserSchema } from '@/shared/types/admin/admin.schema';
+import { verifyUserSchema, deleteUserSchema, userDetailsSchema } from '@/shared/types/admin/admin.schema';
 
 export default (): Router => {
   const app = Router();
@@ -12,7 +12,7 @@ export default (): Router => {
   app.patch('/:user', updateUser);
   app.delete('/user', validateRequest('body', deleteUserSchema), deleteUser);
 
-  app.get('/user/:userid', getUserDetails);
+  app.get('/user/:userid', validateRequest('params', userDetailsSchema), getUserDetails);
 
   return app;
 };
