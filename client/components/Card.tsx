f3cea90ecde08bcdc401b74a3b6bdb8a97a62eeb
vaibhavraj-exe <vraj.exe@gmail.com>
2023-07-04 13:39:11 +0530

    refactor: reduces nesting of props in card component

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -7,23 +7,23 @@ export interface CardPropsType {
   title: string;
   websiteUrl: string;
   manageUrl: string;
-  type: string;
+  cardType: string;
 }
 
-export const Card = ({ cardProps }: { cardProps: CardPropsType }) => {
+export const Card = ({ title, websiteUrl, manageUrl, cardType }: CardPropsType) => {
   const router = useRouter();
 
   return (
     <div className="m-5 flex flex-col w-auto p-12 rounded-3xl bg-[121212] shadow-[inset_-20px_-12px_60px_#58acf525] hover:shadow-[inset_-20px_-12px_60px_#58acf530]">
-      <Link href={cardProps.websiteUrl} className="text-3xl font-bold flex gap-2 pr-20">
-        {cardProps.title}
+      <Link href={websiteUrl} className="text-3xl font-bold flex gap-2 pr-20">
+        {title}
         <ExternalLinkFillIcon />
       </Link>
       <button
         type="button"
         className="text-white bg-[#ff644e] shadow-[inset_-10px_-10px_30px_#12121270] opacity-90 hover:shadow-[inset_-10px_-10px_30px_#12121240] font-medium p-5 mt-10 rounded-xl text-sm w-32 text-center"
         onClick={() => {
-          router.push(cardProps.manageUrl);
+          router.push(manageUrl);
         }}
       >
         Manage
--- client/components/CardsList.tsx
+++ client/components/CardsList.tsx
@@ -15,10 +15,10 @@ const CardList = ({ type, cardList }: CardsListProps) => {
   return (
     <div className="pt-10 flex flex-wrap justify-evenly w-auto min-h-full">
       {cardList.cards.map((card, index) => {
-        if (card.type == type || type == 'all') {
+        if (card.cardType == type || type == 'all') {
           return (
             <div key={index}>
-              <Card cardProps={card} />
+              <Card title={card.title} websiteUrl={card.websiteUrl} manageUrl={card.manageUrl} cardType={card.cardType} />
             </div>
           );
         }
