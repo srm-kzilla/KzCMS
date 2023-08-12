2cd689b7c29be453748c25cd7c34c90048066ede
Jayatri Banerjee <jbanerjeedgp01022003@gmail.com>
2023-08-12 15:59:16 +0530

    feat: add user components

---
+++ client/components/button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export interface ButtonPropsType extends React.HTMLAttributes<HTMLButtonElement> {
+  buttonTitle: string;
+}
+
+const Button = ({ buttonTitle, ...rest }: ButtonPropsType) => {
+  return (
+    <button
+      type='button'
+      {...rest}
+      className='text-[#C4D3E0] bg-gradient-to-tr from-[#5653FF] to-[#B10DFF] hover:bg-gradient-to-br font-medium p-4 rounded-lg text-sm w-32 text-center z-10 duration-300'
+    >
+      {buttonTitle}
+    </button>
+  );
+};
+
+export default Button;
---
+++ client/components/user_cards.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from './button';
+
+interface CardProps {
+  title: string;
+
+  backgroundColor?: string;
+  textColor?: string;
+  shadow?: boolean;
+}
+
+const Card: React.FC<CardProps> = ({
+  title,
+
+  backgroundColor = 'bg-gray-400',
+  textColor = 'text-gray-700',
+  shadow = true,
+}) => {
+  const shadowClasses = shadow ? 'shadow-md' : '';
+
+  return (
+    <div
+      className={`${backgroundColor} ${shadowClasses} rounded-xl border-none mx-2 py-6 my-2 bg-gray-100 bg-opacity-20 shadow-md`}
+    >
+      <div className="w-full px-5">
+        <div className="flex justify-between">
+          <h2 className="text-xl">{title}</h2>
+          <Button buttonTitle="Manage" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
