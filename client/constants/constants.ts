caf203e058e05c66e3348f29223a04a5fb1fe022
vaibhavraj-exe <vraj.exe@gmail.com>
2023-08-15 00:03:36 +0530

    feat: adds signin and signup page

--- client/constants/constants.ts
+++ client/constants/constants.ts
@@ -1,3 +1,6 @@
 export const kZgradientLogoURL = "/srmkzilla-gradient-logo.svg";
 export const kZgradientLogoAlt = "SRMKZILLA";
 export const projectCardButtonTitle = "Manage";
+export const loginButtonTitle = "Log In";
+export const signupButtonTitle = "Sign Up";
+export const deleteButtonTitle = "Delete"
