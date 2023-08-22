234226e9b852229c8357f6a619ec650a0a069510
Aditya Gupta <adityag860libra@gmail.com>
2023-08-22 10:09:05 +0530

    fix: update project title

--- src/shared/types/project/project.schema.ts
+++ src/shared/types/project/project.schema.ts
@@ -25,7 +25,8 @@ export const ProjectSchema = z.object({
 });
 
 export const ProjectDataSchema = z.object({
-  title: z.string(),
+  title: z.string().optional(),
+  newTitle: z.string().optional(),
   description: z.string().optional(),
   image: z.any().optional(),
   link: z.string().url().optional(),
