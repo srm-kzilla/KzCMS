6a389993dfbb6e2e5bbd5103b991ffd94ee23910
Aditya Gupta <adityag860libra@gmail.com>
2023-09-19 11:32:05 +0530

    chore: format env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,13 +1,13 @@
 declare namespace NodeJS {
-    interface ProcessEnv {
-      JWT_SECRET: string;
-      MONGODB_URI: string;
-      PORT: string;
-      LOG_LEVEL: string;
-      NODE_ENV: string;
-      AWS_CLIENT_KEY: string;
-      AWS_CLIENT_SECRET: string;
-      AWS_BUCKET_NAME: string;
-      AWS_REGION: string;
-    }
+  interface ProcessEnv {
+    JWT_SECRET: string;
+    MONGODB_URI: string;
+    PORT: number;
+    LOG_LEVEL: string;
+    NODE_ENV: string;
+    AWS_CLIENT_KEY: string;
+    AWS_CLIENT_SECRET: string;
+    AWS_BUCKET_NAME: string;
+    AWS_REGION: string;
   }
+}
