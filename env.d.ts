f07b568bc2e8a439f94a5ba77ae0d3cc57409ee4
AdityaGupta <99209457+AdityaGupta0001@users.noreply.github.com>
2023-09-22 23:12:18 +0530

    fix MongoDB URI regex (#198)
    * fix: fix main
    
    * chore: rename types to pascal case
    
    * chore: remove test code
    
    * chore: use import alias

--- env.d.ts
+++ env.d.ts
@@ -1,13 +1,6 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    JWT_SECRET: string;
-    MONGODB_URI: string;
-    PORT: number;
-    LOG_LEVEL: string;
-    NODE_ENV: string;
-    AWS_CLIENT_KEY: string;
-    AWS_CLIENT_SECRET: string;
-    AWS_BUCKET_NAME: string;
-    AWS_REGION: string;
+import { EnvSchemaType } from '@/config/index';
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends EnvSchemaType {}
   }
 }
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -16,6 +16,6 @@ export const LINK_REGEX_PATTERN = /https:\/\/(?:[\w.-]+)\.s3\.ap-south-1\.amazon
 
 export const SALT_ROUNDS = 10;
 
-export const EMAIL_REGEX_PATTERN = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+export const EMAIL_REGEX_PATTERN = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;  
 
-export const MONGODB_URI_REGEX_PATTERN = /^mongodb(?:\+srv)?:\/\/(?:(?:\w+):(?:\w+)@)?(?:\w+)(?:\.\w+)*(?:\:\d+)?(?:\/\w+)?(?:\?\w+=\w+)?$/g;
+export const MONGODB_URI_REGEX_PATTERN = /^mongodb(?:\+srv)?:\/\/(?:(?:\w+):(?:\w+)@)?(?:\w+)(?:\.\w+)*(?:\:\d+)?(?:\/\w+)?(?:\?\w+=\w+)?/g;
