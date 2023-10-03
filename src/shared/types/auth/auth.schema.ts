9a6648f9882e616f29f9e8fa3458321758af06f6
Harsh Patel <harshnpatel9@gmail.com>
2023-10-03 14:47:55 +0530

    chore: remove `EmailSchema`

--- src/shared/types/auth/auth.schema.ts
+++ src/shared/types/auth/auth.schema.ts
@@ -1,13 +1,12 @@
 import { z } from 'zod';
-import { EmailSchema } from '@/shared/types';
 
 export const AuthSchema = z.object({
-  email: EmailSchema,
+  email: z.string().email({ message: 'Invalid email format' }),
   password: z.string().min(5, { message: 'Must be 5 characters long or more' }),
 });
 
 export const AuthGetSchema = z.object({
-  email: EmailSchema,
+  email: z.string().email({ message: 'Invalid email format' }),
 });
 
 export type UserSchemaType = z.infer<typeof AuthSchema>;
