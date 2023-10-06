83c1ff99829ffbc764dd81eb7a12bcf47dc5cb21
Aditya Gupta <adityag860libra@gmail.com>
2023-10-07 00:30:45 +0530

    chore: make error codes/messagess consistent

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -7,11 +7,21 @@ export const STATUS = {
 };
 
 export const MESSAGES_TEXT = {
-  FETCH_USERS: 'Users Fetched Successfully',
+  FETCH_USERS: 'User(s) Fetched Successfully',
+  FETCH_USER_DETAILS: 'User Details Fetched Successfully',
+  CREATE_USER: 'User Created Successfully',
+  DELETE_USER: 'User Deleted Successfully',
   UPDATE_USER: 'User Updated Successfully',
-  IMAGE_UPDATED: 'Image Updated Successfully',
   UPDATE_USER_ACCESS: 'User Access Updated Successfully',
+  VERIFY_USER: 'Verification Status Updated Successfully',
+  LOGIN_USER: 'User Logged In Successfully',
+  IMAGE_UPDATED: 'Image Updated Successfully',
+  CREATE_PROJECT: 'Project Created Successfully',
+  CREATE_PROJECT_DATA: 'Project Data Created Successfully',
   UPDATE_PROJECT_ACCESS: 'Project Status Updated',
+  UPDATE_PROJECT_METADATA: 'Project Updated Successfully',
+  DELETE_PROJECT: 'Project Deleted Successfully',
+  DELETE_PROJECT_DATA: 'Project Data Deleted Successfully',
 };
 
 export const LINK_REGEX_PATTERN = /https:\/\/[\w.-]+\.s3\.ap-south-1\.amazonaws\.com\/(.+)/;
