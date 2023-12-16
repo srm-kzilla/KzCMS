099b5387f1839df057fc54b3bc55d97b9ed92ac8
Ash <01tripster@gmail.com>
2023-12-16 22:11:04 +0530

    feat: return token when signup

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,15 +1,17 @@
+import { MESSAGES_TEXT, STATUS } from '@/shared/constants';
 import { UserSchemaType } from '@/shared/types';
 import { NextFunction, Request, Response } from 'express';
 import { handleAddNewUser, handleExistingUser } from './auth.service';
-import { MESSAGES_TEXT, STATUS } from '@/shared/constants';
 
 export const addNewUser = async (req: Request<unknown, unknown, UserSchemaType>, res: Response, next: NextFunction) => {
   const user = req.body;
   try {
     await handleAddNewUser(user);
+    const token = await handleExistingUser(user);
     res.status(STATUS.OK).json({
       success: true,
       message: MESSAGES_TEXT.CREATE_USER,
+      token,
     });
   } catch (error) {
     next(error);
